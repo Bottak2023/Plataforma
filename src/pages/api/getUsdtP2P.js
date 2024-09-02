e06014601d8094754d9f1035b10a01a34e603cfe
Your Name <you@example.com>
2024-09-02 03:48:07 -0400

    DC

--- src/pages/api/getUsdtP2P.js
+++ src/pages/api/getUsdtP2P.js
@@ -1,27 +1,51 @@
 import fetch from 'node-fetch';
-import admin from 'firebase-admin'
+import admin, { credential } from 'firebase-admin'
 import { getDatabase, onValue, set, child, get, remove, update, query, orderByChild, equalTo } from "firebase-admin/database";
 
 
-
+const credentialFB = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.UNIVERSE_DOMAIN
+}
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert({
-            PROJECT_ID: process.env.PROJECT_ID,
-            PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
-            PRIVATE_KEY: process.env.PRIVATE_KEY,
-            CLIENT_EMAIL: process.env.CLIENT_EMAIL,
-            CLIENT_ID: process.env.CLIENT_ID,
-            AUTH_URI: process.env.AUTH_URI,
-            TOKEN_URI: process.env.TOKEN_URI,
-            AUTH_PROVIDER_X509_CERT_URL: process.env.AUTH_PROVIDER_X509_CERT_URL,
-            CLIENT_X509_CERT_URL: process.env.CLIENT_X509_CERT_URL,
-            UNIVERSE_DOMAIN: process.env.UNIVERSE_DOMAIN,
-        }),
+        credential: admin.credential.cert(credentialFB),
         databaseURL: "https://bottak-15afa-default-rtdb.firebaseio.com"
     });
 }
+
+
+
+
+
+
+
+
+
+// console.log({
+
+// "type":process.env.TYPE,
+// "project_id":process.env.PROJECT_ID,
+// "private_key_id":   process.env.PRIVATE_KEY_ID,
+// "private_key": process.env.PRIVATE_KEY,
+// "client_email": process.env.CLIENT_EMAIL,
+// "client_id": process.env.CLIENT_ID,
+// "auth_uri": process.env.AUTH_URI,
+// "token_uri": process.env.TOKEN_URI,
+// "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+// "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+// "universe_domain": process.env.UNIVERSE_DOMAIN
+// })
 const db = getDatabase(admin.apps[0]);
 
 
